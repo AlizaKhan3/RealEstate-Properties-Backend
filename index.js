50fabf2df8304cacfb6e8c732b1c0948fc34c71e
Aliza Khan <alizaadi2@gmail.com>
2025-07-19 01:11:25 +0500

    Installed cors to access frontend vite

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import express from "express"
 import dotenv from "dotenv"
 import appRoutes from "./Routes/appRoutes.js"
 import authRoutes from "./Routes/authRoutes.js"
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
 const PORT = 8000;
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 
 //parsing body;
 app.use(express.json());
